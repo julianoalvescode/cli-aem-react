7d7f9411ebf71fbcc47dd7a29731848cda7dca90
Juliano Alves <contato.julianoalvess@gmail.com>
2022-03-01 17:49:00 -0300

    fix: 🐛 fix build
    fix build

---
+++ dist/controller/usecase/component.d.ts
@@ -0,0 +1,8 @@
+declare class Component {
+    generateComponent({ name, nameProject, }: {
+        name: string;
+        nameProject: string;
+    }): Promise<void>;
+}
+declare const _default: Component;
+export default _default;
--- dist/index.js
+++ dist/index.js
@@ -9,12 +9,9 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
 const commander_1 = require("commander");
-const component_1 = __importDefault(require("./controller/usecase/component"));
+const component_1 = require("./controller/usecase/component");
 const program = new commander_1.Command();
 program
     .name("cli-aem-react")
